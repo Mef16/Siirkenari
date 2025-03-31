0ca7e70444e4fa2ff09a293ebb784207eff5813e
Mef16 <zeyrox6616@gmail.com>
2025-03-31 03:39:57 +0300

    sdfgs

---
+++ script.js
@@ -0,0 +1,4 @@
+// Sayfa yüklendikten sonra kullanıcıya hoş geldin mesajı göster
+document.addEventListener("DOMContentLoaded", function() {
+    alert("Hoş geldiniz! Şiirler Dünyası'na göz atabilirsiniz.");
+});
